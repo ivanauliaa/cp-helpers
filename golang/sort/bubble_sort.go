76b5f9974df4ac52761ba38d6b391a935ca6f671
Ivan Aulia Rahman <ivanar6542@gmail.com>
2022-07-27 13:11:04 +0700

    add quick sort golang

--- golang/sort/bubble_sort.go
+++ golang/sort/bubble_sort.go
@@ -1,6 +1,6 @@
 package golang
 
-func sortInt(arr []int32) []int32 {
+func bubbleSortInt(arr []int32) []int32 {
 	for i := 0; i < len(arr)-1; i++ {
 		for j := i + 1; j < len(arr); j++ {
 			if arr[i] > arr[j] {
---
+++ golang/sort/quick_sort.go
@@ -0,0 +1,34 @@
+package sort
+
+import (
+	"math/rand"
+	"time"
+)
+
+func quickSortInt(a []int32) []int32 {
+	rand.Seed(time.Now().UnixNano())
+
+	if len(a) < 2 {
+		return a
+	}
+
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	a[pivot], a[right] = a[right], a[pivot]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	quickSortInt(a[:left])
+	quickSortInt(a[left+1:])
+
+	return a
+}
