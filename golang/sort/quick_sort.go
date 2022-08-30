875d63962588ae70be84b319d293c6b67c6e5931
Ivan Aulia Rahman <ivanar6542@gmail.com>
2022-08-30 09:44:56 +0700

    add quick sort byte golang

--- golang/sort/quick_sort.go
+++ golang/sort/quick_sort.go
@@ -32,3 +32,31 @@ func quickSortInt(a []int32) []int32 {
 
 	return a
 }
+
+func quickSortByte(a []byte) []byte {
+	rand.Seed(time.Now().UnixNano()) // don't forget to import "math/rand" and "time" package
+
+	if len(a) < 2 {
+		return a
+	}
+
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	a[pivot], a[right] = a[right], a[pivot]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	quickSortByte(a[:left])
+	quickSortByte(a[left+1:])
+
+	return a
+}
